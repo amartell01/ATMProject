bec0b737e31913f48e2724b946812bef243ef2b9
Angie <angelica.martell.c@gmail.com>
2022-08-30 14:28:33 -0700

    changed var names, filled first 3 methods

--- src/ATM.java
+++ src/ATM.java
@@ -1,16 +1,28 @@
 
+import java.util.*; 
+
 public class ATM {
+	
+	private int accountNumber; 
+	private double balance;
+	private HashMap<Integer, Double> accounts = new HashMap<Integer, Double>();
+	
 	public void openAccount (int accountNumber) {
-		
+		this.accountNumber = accountNumber;
+		accounts.put(accountNumber, null);
 	}
 	
-	public void openAccount(int accountNumber, double depostAmt) {
-		
+	public void openAccount(int accountNumber, double depositAmount) {
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+		accounts.put(accountNumber, balance);
 	}
 	
 	//closes the account, only if there is no balance
 	public void closeAccount(int accountNumber) {
-		
+		if (accounts.get(accountNumber) == 0) {
+			accounts.remove(accountNumber, balance);
+		}
 	}
 	
 	//returns a double the exact account value of all desposits into the account
